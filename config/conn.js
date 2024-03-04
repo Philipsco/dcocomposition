5012f08b6b8de9f2383306db0b0c9b0cc5a1c37c
Philip Bagus Kristianto <bagus09051998@gmail.com>
2024-03-04 21:24:02 +0700

    add env

--- config/conn.js
+++ config/conn.js
@@ -1,19 +1,14 @@
+const env = require("dotenv")
+env.config()
+
 const { Pool } = require('pg');
 const pool = new Pool({
-    // username postgres
-    user: 'postgres',
-    // nama database yang akan kita akses 
-    database: 'dcbca_db', 
-    // password postgres
-    password: 'Mahanaim999', 
-    // port postgres
-    port: 9090, 
-    // host dari postgres
-    host: 'localhost', 
-    // connectionTimeoutMillis merupakan optional, 
-    // ini adalah batas waktu untuk memutuskan koneksi 
-    // jika tidak ada aktifitas terhadap database
-    connectionTimeoutMillis: 200 // setelah tidak ada aktifitas selama 200 milisecond maka koneksi akan terputus
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE, 
+    password: process.env.DB_PASSWORD, 
+    port: process.env.DB_PORT, 
+    host: process.env.DB_HOST, 
+    connectionTimeoutMillis: 200
   });
 
 module.exports = {pool}
