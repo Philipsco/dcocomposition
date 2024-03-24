b14b6abc4f07d59d65a5d0eaae82c3c4aa74ed5c
Philip Bagus Kristianto <bagus09051998@gmail.com>
2024-03-24 10:20:03 +0700

    bug fix

--- config/cmd.js
+++ config/cmd.js
@@ -7,7 +7,7 @@ const cmd = {
     help:/\/help/,
     sick: /^sakit\s*(\S*)/,
     onLeave: /^cuti\s*(\S*)/,
-    izin: /^izin\s*(\S*))/,
+    izin: /^izin\s*(\S*)/,
     insertDb : /\/insert_db$/,
     updateDb : /\/update_db$/,
     deleteDb :  /\/delete_db$/,
