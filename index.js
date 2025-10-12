cf79a924f805a3d4c28077eb995354409dd73f01
Philip Bagus Kristianto <32977603+Philipsco@users.noreply.github.com>
2025-10-13 00:15:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,26 +6,26 @@ const port = process.env.PORT || 3000
 const env = require("dotenv")
 env.config()
 
-// const botToken = process.env.BOT_TOKEN
-// const sysoBot = new Bots(botToken, {polling: {
-//   params: {
-//     limit: 100
-//   }
-// }});
+const botToken = process.env.BOT_TOKEN
+const sysoBot = new Bots(botToken, {polling: {
+  params: {
+    limit: 100
+  }
+}});
 
-// const main = () => {
-//   sysoBot.getGreeting()
-//   sysoBot.getQuotes()
-//   sysoBot.getHelp()
-//   sysoBot.getGenerate()
-//   sysoBot.insertDatabase()
-//   sysoBot.deleteDatabase()
-//   sysoBot.updateDatabase()
-//   sysoBot.getEathquake()
-//   sysoBot.sendInfoGempaAuto()
-//   sysoBot.getGeneratePantun()
-//   sysoBot.deleteKomposisi()
-// }
+const main = () => {
+  sysoBot.getGreeting()
+  sysoBot.getQuotes()
+  sysoBot.getHelp()
+  sysoBot.getGenerate()
+  sysoBot.insertDatabase()
+  sysoBot.deleteDatabase()
+  sysoBot.updateDatabase()
+  sysoBot.getEathquake()
+  // sysoBot.sendInfoGempaAuto()
+  sysoBot.getGeneratePantun()
+  sysoBot.deleteKomposisi()
+}
 
 app.use(cors())
 app.use(express.json())
